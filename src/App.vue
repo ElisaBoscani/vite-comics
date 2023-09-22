fc825cf6ba5ba81458cec14c1c821eeb7cb87e2f
Elisa Boscani <elisabosc90@gmail.com>
2023-09-22 15:34:19 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
