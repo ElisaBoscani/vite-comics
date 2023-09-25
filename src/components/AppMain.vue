7387daf0dc2df66aa78104d01966dc9906025383
Elisa Boscani <elisabosc90@gmail.com>
2023-09-25 15:27:24 +0200

    creato file props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,34 +1,37 @@
 <script>
-import MainLink from "./MainLink.vue";
+import MainLink from "../components/MainLink.vue";
+/* import MainProps from "../components/MainProps.vue"; */
+import product from "../product.js";
 export default {
   name: "AppMain",
   data() {
-    return {};
+    return {
+      product: comic,
+    };
   },
   components: {
     MainLink,
+    product,
   },
 };
 </script>
 
 <template>
   <main>
-    <section class="bg_top">
+    <section class="bg_top img_jumb">
       <div class="p-3"></div>
     </section>
-    <MainLink></MainLink>
+    <MainLink> </MainLink>
   </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/partials/variables" as *;
-section {
-  height: 200px;
-}
-
-div {
-  background-image: url("../assets/img/jumbotron.jpg");
+.img_jumb {
+  background-image: url(../assets/img/jumbotron.jpg);
+  background-position: top;
   background-size: cover;
-  height: 200px;
+  background-repeat: no-repeat;
+  padding: 140px;
 }
 </style>
---
+++ src/components/MainProps.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "MainProps",
+  props: {
+    thumb: String,
+    price: String,
+    series: String,
+    type: String,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <img :src="thumb" alt="" />
+    <h3>{{ serie }}</h3>
+  </div>
+</template>
