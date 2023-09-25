c9af25b8bf462a94813951ddb12e2802c7e86c1e
Elisa Boscani <elisabosc90@gmail.com>
2023-09-25 15:59:13 +0200

    creato le card dei fumetti

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,18 @@
 <script>
 import MainLink from "../components/MainLink.vue";
-/* import MainProps from "../components/MainProps.vue"; */
+import MainProps from "../components/MainProps.vue";
 import product from "../product.js";
 export default {
   name: "AppMain",
   data() {
     return {
-      product: comic,
+      product: product,
     };
   },
   components: {
     MainLink,
     product,
+    MainProps,
   },
 };
 </script>
@@ -20,6 +21,15 @@ export default {
   <main>
     <section class="bg_top img_jumb">
       <div class="p-3"></div>
+    </section>
+    <section class="bg">
+      <div class="d-flex flex-wrap container justify-content-center">
+        <MainProps
+          :thumb="product.thumb"
+          :series="product.series"
+          v-for="product in product"
+        ></MainProps>
+      </div>
     </section>
     <MainLink> </MainLink>
   </main>
@@ -33,5 +43,8 @@ export default {
   background-size: cover;
   background-repeat: no-repeat;
   padding: 140px;
+}
+.bg {
+  background-color: $comics_footer;
 }
 </style>
--- src/components/MainProps.vue
+++ src/components/MainProps.vue
@@ -11,8 +11,24 @@ export default {
 </script>
 
 <template>
-  <div>
-    <img :src="thumb" alt="" />
-    <h3>{{ serie }}</h3>
+  <!--   <div class="d-flex col col-md-6">
+    <div class="card">
+      <img :src="thumb" alt="" />
+      <h3>{{ series }}</h3>
+    </div>
+  </div> -->
+
+  <div class="card bg_card" style="width: 18rem">
+    <img :src="thumb" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h3 class="fs-5">{{ series }}</h3>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables" as *;
+img {
+  height: 100%;
+}
+</style>
